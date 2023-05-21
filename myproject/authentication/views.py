f62badeac689aa4397fc3fa7b08a29aa8b09e23d
abdullah <hassanabdullah962@gmail.com>
2023-05-21 10:46:49 +0500

    make changes in signup

--- myproject/authentication/views.py
+++ myproject/authentication/views.py
@@ -7,23 +7,27 @@ def login(request):
 # def signin(request):
 #       return render(request,"authentication/signin.html")
 
-def signup(request):
-     if request.method == "POST":
-      username =request.POST('username')      
-      fname =request.POST('fname')      
-      lname =request.POST('lname')      
-      email =request.POST('email')      
-      password =request.POST('password')      
-      conpassword =request.POST('conpassword') 
 
-      myuser = User.objects.create_user( username, email, password )
-      myuser.first_name = fname     
-      myuser.last_name = lname 
-      myuser.save()
-      messages.success(request , "Your Account has been successfully created.")
-      return redirect('login')  
-      
-      return render(request,"signup.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        conpassword = request.POST.get('conpassword')
+
+        myuser = User.objects.create_user(username=username, email=email, password=password)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+        return redirect('login')
+
+    return render(request, "signup.html")
+
      
 def signout(request):
       pass
